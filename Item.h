1bc432efd4afe4ff748687454499a55bf203092b
ovidiujordache <d00187472@student.dkit.ie>
2023-08-20 17:03:54 +0100

    commenting the CODE

--- Item.h
+++ Item.h
@@ -26,14 +26,7 @@ public:
 	void setNext(Item<T>* pNext) {
 		this->pNext = pNext;
 	}
-	/*
-	bool setNext(Item<T>* pNext) {
-		if (pNext == nullptr)
-			return false;
-		this->pNext = pNext;
-		return true;
-	}
-	*/
+
 	Item* getNext() const { return this->pNext; }
 	void setIndex(int i) {
 		this->index = i;
